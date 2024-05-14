f62f065b0686fed072f63f09cf0c81f2557f9468
mechzz1 <munib.siddiqui110@gmail.com>
2024-05-14 11:50:16 +0500

    bootstrap install and landing page created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Landing from './pages/landing/landing';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <h1>
+        asdads
+      </h1>
+      <Landing/>
     </>
   )
 }
---
+++ frontend/src/pages/landing/landing.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function landing() {
+  return (
+    <>
+        hey there
+    </>
+  )
+}
+
+export default landing
