e06751ac02fbf962161cbadf28505c177152d3a7
mechzz1 <munib.siddiqui110@gmail.com>
2024-05-14 17:25:36 +0500

    login page created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,13 +6,28 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Landing from './pages/landing/landing';
 import About from './pages/about/about';
-
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Login from './pages/login/login';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Landing/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          {/* <Route element={<PrivateRoutes />}>
+            <Route path="dashboard/*" element={<SideBar />}>
+              <Route index path="main" element={<Home />} />
+              <Route  path="app" element={<AppManagement />} />
+              <Route  path="module" element={<ModuleManagement />} />
+              <Route  path="organizations" element={<Organizations />} />
+
+            </Route>
+          </Route> */}
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/pages/login/login.tsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import { z } from 'zod'
+import { useNavigate } from 'react-router-dom'
+import { FieldValues, useForm } from 'react-hook-form'
+import axios from 'axios'
+import { zodResolver } from '@hookform/resolvers/zod';
+import styles from './login.module.css'
+const schema = z.object({
+    userName: z.string().min(3, { message: "User Name is required" }),
+    password: z.string().min(3, { message: "Password is required" }),
+})
+type FormData = z.infer<typeof schema>;
+function login() {
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>({ resolver: zodResolver(schema) });
+    const navigate = useNavigate();
+    const onSubmit = (data: FieldValues) => {
+
+        console.log(data);
+        let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'http://localhost:14000/users/login',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: data
+        };
+
+        const fethUsers = async () => {
+            try {
+                const res = await axios.request(config);
+                const userDataString = JSON.stringify(res.data);
+                sessionStorage.setItem('user', userDataString);
+                setUser(res.data)
+                // toast.success("Success " + `${res.data.message}`)
+                navigate('/dashboard/main');
+            } catch (error) {
+                // toast.error("Error " + `${error}`)
+            }
+        }
+
+        fethUsers();
+    };
+
+    interface userData {
+        id: number,
+        name: string
+    }
+    return (
+        <>
+            <div className={` ${styles.secBg}`}>
+                <div className={styles.midCon}>
+                    <div className="container p-5">
+                        <h4 className='text-center'>
+                            Log in
+                        </h4>
+                        <p className='mb-0 text-center'>
+                            Please enter your details
+                        </p>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div className="row d-flex justify-content-center">
+                                <div className="col-md-12 pt-3">
+                                <label htmlFor="userName">User Name</label>
+
+                                    <input type="text" className="form-control" id="userName" {...register('userName')} />
+                                    {errors.userName && <p className='text-danger'>{errors.userName.message} </p>}
+                                </div>
+                                <div className="col-md-12 pt-3">
+                                <label htmlFor="password">Password</label>
+                                    <input type="password" className="form-control" id="password" {...register('password')} />
+
+                                    {errors.password && <p className='text-danger'>{errors.password.message} </p>}
+                                </div>
+                                <div className="col-md-12 d-flex justify-content-center pt-2">
+                                    <button type="submit" className="btn btn-primary w-100 p-2">Submit</button>
+                                </div>
+
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default login
+
+function setUser(data: any) {
+    throw new Error('Function not implemented.')
+}
